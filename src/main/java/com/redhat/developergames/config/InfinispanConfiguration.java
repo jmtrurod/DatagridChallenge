0640eaa8d273008910a9ca2eac35986d083e19e0
jmtrurod <63926535+jmtrurod@users.noreply.github.com>
2021-10-08 19:27:50 +0200

    Update InfinispanConfiguration.java

--- src/main/java/com/redhat/developergames/config/InfinispanConfiguration.java
+++ src/main/java/com/redhat/developergames/config/InfinispanConfiguration.java
@@ -1,5 +1,10 @@
 package com.redhat.developergames.config;
 
+import org.infinispan.client.hotrod.DefaultTemplate;
+import org.infinispan.client.hotrod.configuration.TransactionMode;
+import org.infinispan.client.hotrod.transaction.lookup.RemoteTransactionManagerLookup;
+import org.infinispan.commons.marshall.JavaSerializationMarshaller;
+import org.infinispan.commons.marshall.ProtoStreamMarshaller;
 import org.infinispan.spring.starter.remote.InfinispanRemoteCacheCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,30 +12,22 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 
 import java.net.URI;
-import java.net.URISyntaxException;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class InfinispanConfiguration {
 
-   @Bean
-   @Order(Ordered.HIGHEST_PRECEDENCE)
-   public InfinispanRemoteCacheCustomizer caches() {
-      return b -> {
-         // Configure the weather cache to be created if it does not exist in the first call
-         URI weatherCacheConfigUri = cacheConfigURI("weatherCache.xml");
-
-         b.remoteCache("weather")
-                 .configurationURI(weatherCacheConfigUri);
-      };
-   }
-
-   private URI cacheConfigURI(String cacheConfigFile) {
-      URI cacheConfigUri;
-      try {
-         cacheConfigUri = this.getClass().getClassLoader().getResource(cacheConfigFile).toURI();
-      } catch (URISyntaxException e) {
-         throw new RuntimeException(e);
-      }
-      return cacheConfigUri;
-   }
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public InfinispanRemoteCacheCustomizer infinispanRemoteCacheCustomizer() {
+        return b -> {
+            b.transaction();
+            b.marshaller(new ProtoStreamMarshaller());
+            b.marshaller(new JavaSerializationMarshaller());
+            b.addJavaSerialWhiteList(".*");
+            b.transaction().transactionTimeout(1, TimeUnit.MINUTES);
+            b.remoteCache("weather")
+                    .templateName(DefaultTemplate.DIST_SYNC);
+        };
+    }
 }
