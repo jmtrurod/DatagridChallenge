8a918657471112a33f088082ddcc9522661f8998
jmtrurod <63926535+jmtrurod@users.noreply.github.com>
2021-10-08 22:17:35 +0200

    Update InfinispanConfiguration.java

--- src/main/java/com/redhat/developergames/config/InfinispanConfiguration.java
+++ src/main/java/com/redhat/developergames/config/InfinispanConfiguration.java
@@ -1,33 +1,65 @@
 package com.redhat.developergames.config;
 
-import org.infinispan.client.hotrod.DefaultTemplate;
-import org.infinispan.client.hotrod.configuration.TransactionMode;
-import org.infinispan.client.hotrod.transaction.lookup.RemoteTransactionManagerLookup;
-import org.infinispan.commons.marshall.JavaSerializationMarshaller;
-import org.infinispan.commons.marshall.ProtoStreamMarshaller;
-import org.infinispan.spring.starter.remote.InfinispanRemoteCacheCustomizer;
+//import org.infinispan.spring.starter.remote.InfinispanRemoteCacheCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 
 import java.net.URI;
-import java.util.concurrent.TimeUnit;
+import java.net.URISyntaxException;
+
+import org.infinispan.spring.starter.embedded.InfinispanCacheConfigurer;
+import org.infinispan.spring.starter.embedded.InfinispanGlobalConfigurationCustomizer;
+import org.infinispan.spring.starter.embedded.InfinispanConfigurationCustomizer;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.cache.CacheMode;
+import org.infinispan.eviction.EvictionType;
 
 @Configuration
 public class InfinispanConfiguration {
 
-    @Bean
-    @Order(Ordered.HIGHEST_PRECEDENCE)
-    public InfinispanRemoteCacheCustomizer infinispanRemoteCacheCustomizer() {
-        return b -> {
-            b.transaction();
-            b.marshaller(new ProtoStreamMarshaller());
-            b.marshaller(new JavaSerializationMarshaller());
-            b.addJavaSerialWhiteList(".*");
-            b.transaction().transactionTimeout(1, TimeUnit.MINUTES);
-            b.remoteCache("weather")
-                    .templateName(DefaultTemplate.DIST_SYNC);
+   /*@Bean
+   @Order(Ordered.HIGHEST_PRECEDENCE)
+   public InfinispanRemoteCacheCustomizer caches() {
+      return b -> {
+         // Configure the weather cache to be created if it does not exist in the first call
+         URI weatherCacheConfigUri = cacheConfigURI("weatherCache.xml");
+
+         b.remoteCache("weather")
+                 .configurationURI(weatherCacheConfigUri);
+      };
+   }
+
+   private URI cacheConfigURI(String cacheConfigFile) {
+      URI cacheConfigUri;
+      try {
+         cacheConfigUri = this.getClass().getClassLoader().getResource(cacheConfigFile).toURI();
+      } catch (URISyntaxException e) {
+         throw new RuntimeException(e);
+      }
+      return cacheConfigUri;
+   }*/
+
+   // Configuración para usar datagrid en local a continuación
+   @Bean
+    public InfinispanCacheConfigurer cacheConfigurer() {
+        return manager -> {
+            final org.infinispan.configuration.cache.Configuration ispnConfig = new ConfigurationBuilder()
+                            .clustering()
+                            .cacheMode(CacheMode.LOCAL)
+                            .build();
+
+            manager.defineConfiguration("local-sync-config", ispnConfig);
         };
+    }
+
+    @Bean(name = "testCache")
+    public org.infinispan.configuration.cache.Configuration testCache() {
+        return new ConfigurationBuilder()
+            //.read(baseCache)
+            //.memory().size(1000L)
+            //.memory().evictionType(EvictionType.COUNT)
+            .build(); // MUY IMPORTANTE, EL PARÁMETRO EVICTION ES EL QUE ESTABLECE CUÁNTO TIEMPO PUEDO PERMANECER ALMACENADO UN VALOR EN LA MEMORIA
     }
 }
